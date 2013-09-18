f9caf7f936b460e9c3769707f0d4bc799853ca6a
gael.lalire <gael.lalire@1ef5f59a-b6c6-fdd3-08db-848700544892>
2013-09-18 20:34:54 +0000

    [maven-release-plugin]  copy for tag vestige-1.0.0

---
+++ vestige/tags/vestige-1.0.0/admin/ssh/src/main/java/com/googlecode/vestige/admin/ssh/SFTP.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of Vestige.
+ *
+ * Vestige is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vestige is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vestige.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.vestige.admin.ssh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.sshd.SshServer;
+import org.apache.sshd.common.NamedFactory;
+import org.apache.sshd.server.Command;
+import org.apache.sshd.server.UserAuth;
+import org.apache.sshd.server.auth.UserAuthNone;
+import org.apache.sshd.server.command.ScpCommandFactory;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.sftp.SftpSubsystem;
+
+public class SFTP {
+
+    public static void main(final String[] args) throws Exception {
+        SshServer sshd = SshServer.setUpDefaultServer();
+        sshd.setPort(1111);
+        sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"));
+
+        List<NamedFactory<UserAuth>> userAuthFactories = new ArrayList<NamedFactory<UserAuth>>();
+        userAuthFactories.add(new UserAuthNone.Factory());
+        sshd.setUserAuthFactories(userAuthFactories);
+
+        sshd.setCommandFactory(new ScpCommandFactory());
+
+        List<NamedFactory<Command>> namedFactoryList = new ArrayList<NamedFactory<Command>>();
+        namedFactoryList.add(new SftpSubsystem.Factory());
+        sshd.setSubsystemFactories(namedFactoryList);
+
+        sshd.start();
+    }
+
+}
