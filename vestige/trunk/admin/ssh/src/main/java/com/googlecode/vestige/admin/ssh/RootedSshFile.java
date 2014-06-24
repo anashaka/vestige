b75901ec9083cac035e7306f5917edcf67f4bbd2
gael.lalire <gael.lalire@1ef5f59a-b6c6-fdd3-08db-848700544892>
2014-06-24 22:20:14 +0000

    mina version

--- vestige/trunk/admin/ssh/src/main/java/com/googlecode/vestige/admin/ssh/RootedSshFile.java
+++ vestige/trunk/admin/ssh/src/main/java/com/googlecode/vestige/admin/ssh/RootedSshFile.java
@@ -18,6 +18,10 @@
 package com.googlecode.vestige.admin.ssh;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.sshd.common.file.nativefs.NativeSshFile;
 
@@ -26,8 +30,45 @@ import org.apache.sshd.common.file.nativefs.NativeSshFile;
  */
 public class RootedSshFile extends NativeSshFile {
 
+    private Map<Attribute, Object> attributes = new HashMap<Attribute, Object>();
+
     public RootedSshFile(final RootedFileSystemView rootedFileSystemView, final String fileName, final File file, final String userName) {
         super(rootedFileSystemView, fileName, file, userName);
+    }
+
+    @Override
+    public void setAttribute(final Attribute attribute, final Object value) throws IOException {
+        switch (attribute) {
+        case LastModifiedTime:
+            file.setLastModified((Long) value);
+            break;
+        default:
+            break;
+        }
+        attributes.put(attribute, value);
+    }
+
+    @Override
+    public void setAttributes(final Map<Attribute, Object> attributes) throws IOException {
+        for (Entry<Attribute, Object> entry : attributes.entrySet()) {
+            setAttribute(entry.getKey(), entry.getValue());
+        }
+    }
+
+    @Override
+    public Object getAttribute(final Attribute attribute, final boolean followLinks) throws IOException {
+        Object object = this.attributes.get(attribute);
+        if (object != null) {
+            return object;
+        }
+        return super.getAttribute(attribute, followLinks);
+    }
+
+    @Override
+    public Map<Attribute, Object> getAttributes(final boolean followLinks) throws IOException {
+        Map<Attribute, Object> attributes = super.getAttributes(followLinks);
+        attributes.putAll(this.attributes);
+        return attributes;
     }
 
 }
